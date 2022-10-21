5409e3aacadc83f303d3e8eff571196894cb4b99
KaterinaShpilevskaya <katyashpilevskaya@gmail.com>
2022-10-21 13:36:51 +0300

    added tabs.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useState } from "react";
 
 //@ts-ignore
 import styles from "./App.module.css";
+import { BurgerIcon, CloseIcon } from "./Assets/icons";
 import Burger from "./Components/Burger";
 import Button, { ButtonTypes } from "./Components/Button";
-import Tabs from "./Components/Tabs";
+import { Tabslist } from "./Components/TabsList";
 import Title from "./Components/Title";
 import UserName from "./Components/UserName";
 
 const App = () => {
+
+  const [isOpened, setOpened] = useState(false);
+  
   return (
     <div className={styles.container}>
       <Button
@@ -20,17 +25,23 @@ const App = () => {
         title={"Secondary"}
         type={ButtonTypes.Secondary}
         onClick={() => alert("secondary")}
-        className = {'secondaryFromProp'}
+        className={"secondaryFromProp"}
       />
       <Button
         title={"Error"}
         type={ButtonTypes.Error}
         onClick={() => alert("error")}
       />
-      <UserName username = {'Artem_Malkin'} />
-      <Title title = {'Sign In'} />
+      <UserName username={"Artem_Malkin"} />
+      <Title title={"Sign In"} />
       <Burger />
-      
+      <Button
+        title={!isOpened ? <BurgerIcon /> : <CloseIcon />}
+        type={ButtonTypes.Primary}
+        className={styles.burgerButton}
+        onClick={() => setOpened(!isOpened)}
+      />
+      <Tabslist  />
     </div>
   );
 };
