64fb87690df9637da9ed714f19ded3ea0905f568
KaterinaShpilevskaya <katyashpilevskaya@gmail.com>
2022-11-20 16:55:24 +0300

    Redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import ThemeProvider from "./Context/Theme";
 import { Theme } from "./Constants/@types";
 import Router from "./Pages/Router";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const App = () => {
   const [theme, setTheme] = useState(Theme.Light);
@@ -12,9 +14,11 @@ const App = () => {
   };
 
   return (
+    <Provider store={store}>
     <ThemeProvider theme={theme} onChangeTheme={onChangeTheme}>
       <Router />
     </ThemeProvider>
+    </Provider>
   );
 };
 
