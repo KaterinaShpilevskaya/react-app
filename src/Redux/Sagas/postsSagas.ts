63db73f00216bf47554914cd4181ee9870a70c05
KaterinaShpilevskaya <katyashpilevskaya@gmail.com>
2022-12-18 23:38:18 +0300

    getting posts on a page with posts

--- src/Redux/Sagas/postsSagas.ts
+++ src/Redux/Sagas/postsSagas.ts
@@ -1,9 +1,9 @@
 import { takeLatest, all, call, put } from "redux-saga/effects";
-import { PayloadAction } from "@reduxjs/toolkit";
 import { getPosts, setPosts } from "../Reducers/postsReducer";
 import API from "../utils/api";
 
-function* getPostsWorker(action: PayloadAction<undefined>) {
+
+function* getPostsWorker() {
   const { ok, data, problem } = yield call(API.getAllPosts);
   if (ok && data) {
     yield put(setPosts(data.results));
